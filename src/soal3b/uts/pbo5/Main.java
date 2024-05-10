1088cf7f7fccdbedf878c1539f730e06eef6d7aa
RivaldoPardede <rivaldopardede3@gmail.com>
2024-05-10 22:28:38 +0700

    implementasi class Employee

--- src/soal3b/uts/pbo5/Main.java
+++ src/soal3b/uts/pbo5/Main.java
@@ -1,19 +1,23 @@
 package soal3b.uts.pbo5;
 
+import soal3b.uts.pbo5.util.*;
+
 public class Main {
     public static void main(String[] args) {
-//        Employee e1 = new Employee(8, "Xavier", "Valave", 2500);
-//        System.out.println(e1);
-//
-//        e1.setSalary(999); System.out.println(e1);
-//        System.out.println("id is: " + e1.getID());
-//        System.out.println("firstname is: " + e1.getFirstName());
-//        System.out.println("lastname is: " + e1.getLastName());
-//        System.out.println("salary is: " + e1.getSalary());
-//        System.out.println("name is: " + e1.getName());
-//        System.out.println("annual salary is: " + e1.getAnnualSalary());
-//
-//        System.out.println(e1.raiseSalary(10));
-//        System.out.println(e1);
+        Employee e1 = new Employee(8, "Xavier", "Valave", 2500);
+        System.out.println(e1);
+
+        e1.setSalary(999);
+        System.out.println(e1);
+
+        System.out.println("id is: " + e1.getID());
+        System.out.println("firstname is: " + e1.getFirstName());
+        System.out.println("lastname is: " + e1.getLastName());
+        System.out.println("salary is: " + e1.getSalary());
+        System.out.println("name is: " + e1.getName());
+        System.out.println("annual salary is: " + e1.getAnnualSalary());
+
+        System.out.println(e1.raiseSalary(10));
+        System.out.println(e1);
     }
 }
--- src/soal3b/uts/pbo5/util/Employee.java
+++ src/soal3b/uts/pbo5/util/Employee.java
@@ -1,4 +1,40 @@
 package soal3b.uts.pbo5.util;
 
+import java.text.MessageFormat;
+
 public class Employee {
+    private final int id;
+    private int salary;
+    private final String firstName, lastName;
+
+    public Employee (int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getID() { return id; }
+
+    public String getFirstName() { return firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public String getName() { return firstName + " " + lastName; }
+
+    public int getSalary() { return salary; }
+
+    public void setSalary(int salary) { this.salary = salary; }
+
+    public int getAnnualSalary() { return salary*12; }
+
+    public int raiseSalary(int percent) {
+        int newSalary = (int) (salary + salary*(percent/100.0));
+        setSalary(newSalary);
+        return newSalary;
+    }
+
+    public String toString() {
+        return MessageFormat.format("Employee[id={0},name={1} {2},salary={3, number, #}]", id, firstName, lastName, salary);
+    }
 }
