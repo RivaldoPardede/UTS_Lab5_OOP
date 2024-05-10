e14ba16a528805d3728b0841acf3ab96793ae2dd
RivaldoPardede <rivaldopardede3@gmail.com>
2024-05-10 17:58:08 +0700

    Penjelasan naratif berbentuk komentar

--- src/soal2b/uts/pbo5/Main.java
+++ src/soal2b/uts/pbo5/Main.java
@@ -1,5 +1,6 @@
-package soal2b.uts.pbo5;
+package soal2b.uts.pbo5;//penamaan package
 
+//import utilitas dan package yang diperlukan
 import java.util.Scanner;
 import soal2b.uts.pbo5.util.*;
 
@@ -7,16 +8,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("==== Welcome to AnagramChecker Program =====\n");
 
+        //membuat objek scanner untuk menerima inputan pengguna
         Scanner scanner = new Scanner(System.in);
+        //membuat objek checker untuk mengecek status anagram
         anagramChecker checker = new anagramChecker();
 
         System.out.print("Input word 1: ");
         String word1 = scanner.nextLine();
+        //menerima inputan untuk kata/frasa pertama
 
         System.out.print("Input word 2: ");
         String word2 = scanner.nextLine();
+        //menerima inputan untuk kata/frasa kedua
 
         boolean anagramStatus = checker.areAnagrams(word1, word2);
+        //variable anagramStatus untuk menampung hasil cek dari 2 kata/frasa
 
         System.out.println("Word 1 : " + word1);
         System.out.println("Word 2 : " + word2);
--- src/soal2b/uts/pbo5/util/anagramChecker.java
+++ src/soal2b/uts/pbo5/util/anagramChecker.java
@@ -1,17 +1,24 @@
-package soal2b.uts.pbo5.util;
+package soal2b.uts.pbo5.util;//penamaan package
 
+//import utilitas yang diperlukan
 import java.util.Arrays;
-public class anagramChecker {
+public class anagramChecker { //membuat kelas baru bernaam anagramChecker
+
+    //membuat method dengan nilai balika boolean untuk mengecek hasil anagram
     public boolean areAnagrams(String word1, String word2) {
+        //mengubah semua huruf dalam kata/frasa menjadi huruf kecil untuk memudahkan pengecekan
         word1 = word1.toLowerCase();
         word2 = word2.toLowerCase();
 
+        //mengubah kata/frasa dari huruf kecil menjadi kumpulan array karakter
         char[] arrayChar1 = word1.toCharArray();
         char[] arrayChar2 = word2.toCharArray();
 
+        //mengurutkan kata/frasa tersebut berdasarkan kode ASCII terkecil->terbesar
         Arrays.sort(arrayChar1);
         Arrays.sort(arrayChar2);
 
+        //kemudian bandingkan 2 kata/frasa tersebut, jika memang identik, maka mereka adalah anagram
         return Arrays.equals(arrayChar1, arrayChar2);
     }
 }
