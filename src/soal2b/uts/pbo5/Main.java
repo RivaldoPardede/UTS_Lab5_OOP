4bc2d8da0ad37719b8820bb0b6498f6f8f0079c3
RivaldoPardede <rivaldopardede3@gmail.com>
2024-05-10 17:52:26 +0700

    implementasi anagramChecker di main program

--- src/soal2b/uts/pbo5/Main.java
+++ src/soal2b/uts/pbo5/Main.java
@@ -1,18 +1,26 @@
 package soal2b.uts.pbo5;
 
 import java.util.Scanner;
+import soal2b.uts.pbo5.util.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("==== Welcome to AnagramChecker Program =====\n");
 
         Scanner scanner = new Scanner(System.in);
+        anagramChecker checker = new anagramChecker();
 
-        System.out.println("Input word 1: ");
+        System.out.print("Input word 1: ");
         String word1 = scanner.nextLine();
 
-        System.out.println("Input word 2: ");
+        System.out.print("Input word 2: ");
         String word2 = scanner.nextLine();
 
+        boolean anagramStatus = checker.areAnagrams(word1, word2);
+
+        System.out.println("Word 1 : " + word1);
+        System.out.println("Word 2 : " + word2);
+
+        System.out.println("\nAnagram Status: " + anagramStatus);
     }
 }
