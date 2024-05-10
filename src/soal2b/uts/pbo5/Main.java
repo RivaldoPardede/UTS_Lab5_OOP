e24565b82d3401a4d78bbccc9a3a42fd3bd5309e
RivaldoPardede <rivaldopardede3@gmail.com>
2024-05-10 17:31:22 +0700

    implementasi class anagramChecker dan methodnya

--- src/soal2b/uts/pbo5/Main.java
+++ src/soal2b/uts/pbo5/Main.java
@@ -1,6 +1,18 @@
 package soal2b.uts.pbo5;
 
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("==== Welcome to AnagramChecker Program =====\n");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input word 1: ");
+        String word1 = scanner.nextLine();
+
+        System.out.println("Input word 2: ");
+        String word2 = scanner.nextLine();
+
+    }
 }
--- src/soal2b/uts/pbo5/util/anagramChecker.java
+++ src/soal2b/uts/pbo5/util/anagramChecker.java
@@ -1,5 +1,17 @@
 package soal2b.uts.pbo5.util;
 
+import java.util.Arrays;
 public class anagramChecker {
+    public boolean areAnagrams(String word1, String word2) {
+        word1 = word1.toLowerCase();
+        word2 = word2.toLowerCase();
 
+        char[] arrayChar1 = word1.toCharArray();
+        char[] arrayChar2 = word2.toCharArray();
+
+        Arrays.sort(arrayChar1);
+        Arrays.sort(arrayChar2);
+
+        return Arrays.equals(arrayChar1, arrayChar2);
+    }
 }
